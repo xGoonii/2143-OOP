cb2d93be9ce4786a84421d766043a2404b940b7a
xGoonii <jvaldezjr9@icloud.com>
2026-04-04 20:09:23 +0000

    finishing up, saving point to eat

--- assignments/P02/Args.cpp
+++ assignments/P02/Args.cpp
@@ -1,12 +1,12 @@
 #include "Args.h"
-#include <stdexepct>
+#include <stdexcept>
 #include <iostream>
 #include <string>
 
 
 using namespace std; 
 
-Args args::parse(int argc, char** argv[]){
+Args Args::parse(int argc, char* argv[]){
     Args args;
 
     if (argc < 3){
@@ -32,7 +32,8 @@ Args args::parse(int argc, char** argv[]){
         else if (flag == "--flipV" || flag == "-v"){
             args.flipV = true;
         }
-        else if (flag == "--brightness" || flag == "-b"){
+
+        else if (flag == "--brighten" || flag == "-b"){
             // checks if following value is there. i+1 > argc (cap) means out of bounds
             if (i + 1 >= argc) {
                 throw runtime_error("--brighten requires a value");
@@ -46,14 +47,39 @@ Args args::parse(int argc, char** argv[]){
                 n = stoi(value);
             } 
             catch (...){
-                throw runtime_error("invalid integer for --brightness: " + value):
+                throw runtime_error("invalid integer for --brightness: " + value);
             }
             if(n < -225 || n > 255) {
                 throw runtime_error("brighten must be in [-255, 255]");
             }
+             args.use_brighten = true;
+             args.brighten = n; 
+        }
+        else if (flag == "--rotate" || flag == "-r"){
+                if(i + 1 >= argc){
+                    throw runtime_error("--rotate requires a value ")
+                }
+                string value = argv[++i]; //moves to following value 
+            if (value[0]== '-'){
+                throw runtime_error ("--brighten expected a value got option " + value);
+            }
+            int n; 
+            try { 
+                n = stoi(value);
+            } 
+            catch (...){
+                throw runtime_error("invalid integer for --rotate: " + value);
+            }
+            if(!(n == 0 || n == 90 || n == 180 || n == 270)) {
+                throw runtime_error("rotation must be in [0, 90, 19-, 270]");
+            }
+            }
 
-            args.use_brighten = true;
-            args.brighten = n; 
+            args.use_rotate = true;
+            args.rotate = n; 
+        }
+        else {
+            throw runtime_error("Unknown option" + flag);
         }
     
     }
