e9f17bb81ce9903ba7fdce08275e7af3f79f2c9e
xGoonii <jvaldezjr9@icloud.com>
2026-04-06 18:48:02 +0000

    Notes for std Except

--- Notes/includeExcept.cpp
+++ Notes/includeExcept.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 #include <stdexcept> //needed lib for any of the keywords to work
 
+using namespace std;
+
+/* Think of the code like this TRY (your code) 
+IF error happens Throw an error message. 
+when an error message is throw something has to catch it(the end)
+The catch is what is displayed along with the error.
+
+The "e.what()" is the message that is thrown*/
+
+
 int main(){
     try{
         int age;
 
-        cout << "enter your age";
+        cout << "enter your age ";
         cin >> age;
 
         if(age<0){
-            throw runtime_error("Age cannot be negative!!")
+            throw runtime_error("Age cannot be negative!!");
         }
 
         if(age>122){
-            throw runtime_error("Nice try, you are not the oldest person to ever live")
-        };
+            throw runtime_error("Nice try, you are not the oldest person to ever live");
+        }
         cout << "Your age is : " << age << endl;
     }
     catch(exception& e){
         cout << "Error: " << e.what() << endl;
     }
-return 0
+return 0;
 }
 
 /*The reason for this is telling your compiler to do something else INSTEAD of crashing when an error occurs 
@@ -38,4 +48,6 @@ try{
     }
 catch(exception& e) {  
         cout << "Error: " << e.what() << endl;
-    }
+    }*/ 
+
+
