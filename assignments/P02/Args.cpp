7c5a152d732a8ae99bd3b1ac81a4aa5b414d8da6
xGoonii <jvaldezjr9@icloud.com>
2026-04-06 15:06:13 +0000

    not sure what its making me push

--- assignments/P02/Args.cpp
+++ assignments/P02/Args.cpp
@@ -10,7 +10,7 @@ Args Args::parse(int argc, char* argv[]){
     Args args;
 
     if (argc < 3){
-        throw runtime_error("missing required arguements");
+        throw runtime_error("missing required arguments");
     }
 
     args.input = argv[1];
@@ -47,9 +47,9 @@ Args Args::parse(int argc, char* argv[]){
                 n = stoi(value);
             } 
             catch (...){
-                throw runtime_error("invalid integer for --brightness: " + value);
+                throw runtime_error("invalid integer for --brighten: " + value);
             }
-            if(n < -225 || n > 255) {
+            if(n < -255 || n > 255) {
                 throw runtime_error("brighten must be in [-255, 255]");
             }
              args.use_brighten = true;
@@ -57,11 +57,11 @@ Args Args::parse(int argc, char* argv[]){
         }
         else if (flag == "--rotate" || flag == "-r"){
                 if(i + 1 >= argc){
-                    throw runtime_error("--rotate requires a value ")
+                    throw runtime_error("--rotate requires a value");
                 }
                 string value = argv[++i]; //moves to following value 
             if (value[0]== '-'){
-                throw runtime_error ("--brighten expected a value got option " + value);
+                throw runtime_error ("--rotate expected a value but got option " + value);
             }
             int n; 
             try { 
@@ -71,15 +71,13 @@ Args Args::parse(int argc, char* argv[]){
                 throw runtime_error("invalid integer for --rotate: " + value);
             }
             if(!(n == 0 || n == 90 || n == 180 || n == 270)) {
-                throw runtime_error("rotation must be in [0, 90, 19-, 270]");
+                throw runtime_error("rotation must be in [0, 90, 180, 270]");
             }
-            }
-
             args.use_rotate = true;
-            args.rotate = n; 
+            args.rotate = n;
         }
         else {
-            throw runtime_error("Unknown option" + flag);
+            throw runtime_error("Unknown option: " + flag);
         }
     
     }
